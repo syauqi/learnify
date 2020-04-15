1f21730bd07c0a9bfbf68a81eb5e95fa62006637
syauqizaidan <zaidanline67@Gmail.com>
2020-04-15 17:31:08 +0700

    Bahasa Inggris XI - Clean Code - Fix CSS Error

--- application/views/materi/inggris-xi.php
+++ application/views/materi/inggris-xi.php
@@ -16,11 +16,12 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 <html lang="en">
 
 <head>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" href="<?=base_url('assets/')?>img/favicon.png" type="image/png">
-    <title>Learnify - Belajar Dimana Saja & Kapan Saja !</title>
+    <title>Bahasa Inggris | Kelas XI - Learnify</title>
     <!-- Bootstrap CSS -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/bootstrap.css">
@@ -31,19 +32,22 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/nice-select/css/nice-select.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/animate-css/animate.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/popup/magnific-popup.css">
-    <!-- main css -->
+    <!-- Main CSS -->
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/style.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/materi_style.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/responsive.css">
+    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+    <!-- Library -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.4/dist/sweetalert2.all.min.js"></script>
 
 </head>
 
-<body style="overflow-x:hidden;background-color:#fbf9fa">
+<body style="overflow-x:hidden;background-color:#fbf9fa;">
 
-    <!--================Header Menu Area =================-->
+
+    <!-- Start Navigation Bar -->
     <header class="header_area" style="background-color: white !important;">
         <div class="main_menu">
             <nav class="navbar navbar-expand-lg navbar-light">
@@ -61,55 +65,58 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                         <ul class="nav navbar-nav menu_nav ml-auto">
-                            <li class="nav-item active"><a class="nav-link" href="<?=base_url('user')?>">Beranda</a>
-                            </li>
-
-                            </li>
-                            <li class="nav-item"><a class="nav-link" href="javascript:void(0)">Hai, <?php
+                            <li class="nav-item "><a class="nav-link" href="javascript:void(0)">Hai, <?php
 $data['user'] = $this->db->get_where('siswa', ['email' =>
     $this->session->userdata('email')])->row_array();
 echo $data['user']['nama'];
-?>
-                            <li class="nav-item "><a class="nav-link text-danger"
+?></a>
+                            </li>
+                            <li class="nav-item active"><a class="nav-link" href="<?=base_url('user')?>">Beranda</a>
+                            </li>
+                            <li class=" nav-item "><a class=" nav-link text-danger"
                                     href="<?=base_url('welcome/logout')?>">Log Out</a>
                             </li>
-                    </div></a></li>
-
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
-        </div>
-        </nav>
+            </nav>
         </div>
     </header>
-    <!--================Header Menu Area =================-->
+    <!-- End Navigation Bar -->
+
+
+    <!-- Start Greeting Cards -->
     <div class="container">
         <div class="bg-white mx-auto p-4 buat-text" data-aos="fade-down" data-aos-duration="1400"
             style="width: 100%; border-radius:10px;">
-            <div class="row" style="color: black; font-family: 'poppins">
+            <div class="row" style="color: black; font-family: 'poppins';">
                 <div class="col-md-12 mt-1 text-center">
-                    <h1 class="display-4" style="color: black; font-family:'poppins'" data-aos="fade-down"
-                        data-aos-duration="1400">Silahkan pilih materi yang akan kamu pelajari.
+                    <h1 class="display-4" data-aos="fade-down" data-aos-duration="1400">Silahkan pilih materi yang akan
+                        kamu pelajari !
                     </h1>
                     <h4 data-aos="fade-down" data-aos-duration="1700"><?php
 $data['user'] = $this->db->get_where('siswa', ['email' =>
     $this->session->userdata('email')])->row_array();
 echo $data['user']['nama'];
-?> 😄</h3>
-                        <p>Bahasa Inggris - Kelas XI</p>
-                        <p data-aos="fade-down" data-aos-duration="1800">Silahkan pilih materi yang akan kamu akses
-                            dibawah
-                            ini!
-                        </p>
+?> - Learnify Students</h4>
+                    <p>Bahasa Inggris - Kelas XI</p>
+                    <hr width="80%">
+                    <p data-aos="fade-down" class="font-weight-bold" data-aos-duration="1800">Silahkan pilih materi yang
+                        akan kamu akses
+                        dibawah
+                        ini!
+                    </p>
                 </div>
             </div>
         </div>
     </div>
+    <!-- End Greeting Cards -->
 
+
+    <!-- Start Lesson Cards -->
     <div class="container">
         <div class="row mt-4">
-
             <?php foreach ($materi as $u) {?>
-
             <div class="col-md-6 mb-4" data-aos="fade-right" data-aos-duration="1200">
                 <div class="card materi w-150 border-0">
                     <div class="card-body p-5">
@@ -125,25 +132,15 @@ echo $data['user']['nama'];
             <?php }?>
         </div>
     </div>
-
+    <!-- End Lesson Cards -->
 
 
     <br>
 
 
-    <script>
-    $('.tab1_btn').prop('disabled', !$('.tab1_chk:checked')
-        .length); //initially disable/enable button based on checked length
-    $('input[type=checkbox]').click(function() {
-        if ($('.tab1_chk:checkbox:checked').length > 0) {
-            $('.btn-submit').prop('disabled', false);
-        } else {
-            $('.btn-submit').prop('disabled', true);
-        }
-    });
-    </script>
-
+    <!-- Start Animate On Scroll -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
     AOS.init();
     </script>
+    <!-- End Animate On Scroll -->
