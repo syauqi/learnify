3b9c41d48df118a772f29923309e52988bfebc74
syauqizaidan <zaidanline67@Gmail.com>
2020-04-15 18:08:23 +0700

    Agama x

--- application/views/materi/belajar.php
+++ application/views/materi/belajar.php
@@ -45,7 +45,7 @@ echo $data['user']['nama'];
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <!-- Library -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.4/dist/sweetalert2.all.min.js"></script>
-    <script src="//cdn.jsdelivr.net/npm/afterglowplayer@1.x"></script>
+    <script src="https://cdn.jsdelivr.net/npm/afterglowplayer@1.x"></script>
 
 </head>
 
