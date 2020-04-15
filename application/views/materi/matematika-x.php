b6a5f1f182a827b27656f302f902f6f63f24317f
syauqizaidan <zaidanline67@Gmail.com>
2020-04-15 17:20:34 +0700

    Matematika XI - Clean Code - Fix Some Error

--- application/views/materi/matematika-x.php
+++ application/views/materi/matematika-x.php
@@ -32,7 +32,7 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/nice-select/css/nice-select.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/animate-css/animate.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/popup/magnific-popup.css">
-    <!-- Main css -->
+    <!-- Main CSS -->
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/style.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/materi_style.css">
     <link rel="stylesheet" href="<?=base_url('assets/')?>css/responsive.css">
