192c59d235f2c74752d0ff853a349a3fdf5258cd
syauqizaidan <zaidanline67@Gmail.com>
2020-04-15 20:17:41 +0700

    Autofill Fixed

--- autofill.php
+++ autofill.php
@@ -1,17 +1,3 @@
-<!--
-@Project: Learnify
-@Author/Programmer: Syauqi Zaidan Khairan Khalaf
-@URL: syauqi.js.org
-Author E-mail: Zaidanline67@Gmail.com
-
-@About-Learnify :
-Web Edukasi Open Source yang
-dibuat oleh Syauqi Zaidan Khairan Khalaf.
-Learnify adalah Web edukasi yang dilengkapi video, materi, dan soal ( Coming soon )
-yang didesign semenarik dan sesimple mungkin. Learnify dibuat ditujukan agar para siswa
-dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
--->
-
 <?php
 include 'koneksi.php';
 $nama_guru = $_GET['nama_guru'];
