86dfbcc8166860fdc9eb0a9b56e54baff4d61ffb
syauqizaidan <zaidanline67@Gmail.com>
2020-04-15 18:11:58 +0700

    Minor

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,17 @@
+<!--
+@Project: Learnify
+@Author/Programmer: Syauqi Zaidan Khairan Khalaf
+@URL: syauqi.js.org
+Author E-mail: Zaidanline67@Gmail.com
+
+@About-Learnify :
+Web Edukasi Open Source yang
+dibuat oleh Syauqi Zaidan Khairan Khalaf.
+Learnify adalah Web edukasi yang dilengkapi video, materi, dan soal ( Coming soon )
+yang didesign semenarik dan sesimple mungkin. Learnify dibuat ditujukan agar para siswa
+dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
+-->
+
 <?php
 $koneksi = mysqli_connect("localhost", "root", "", "learnify");
 
