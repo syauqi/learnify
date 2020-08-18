6509ee457c06698f27791fb96a6f1916dde29d6c
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-08-18 16:24:32 +0700

    Admin dashboard final touch 🎊

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -19,16 +19,13 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Admin Dashboard - Learnify</title>
     <!-- General CSS Files -->
-    <link rel="icon" href="<?=base_url('assets/')?>img/favicon.png" type="image/png">
+    <link rel="icon" href="<?= base_url('assets/') ?>img/favicon.png" type="image/png">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:500,600,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
-        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" <!--
-        Template CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/')?>stisla-assets/css/style.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>stisla-assets/css/components.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous" <!-- Template CSS -->
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>stisla-assets/css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>stisla-assets/css/components.css">
 </head>
 
 <body>
@@ -40,26 +37,22 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
             <nav class="navbar navbar-expand-lg main-navbar">
                 <form class="form-inline mr-auto">
                     <ul class=" navbar-nav mr-3">
-                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i
-                                    class="fas fa-bars"></i></a>
+                        <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a>
                         </li>
                     </ul>
                 </form>
                 <ul class="navbar-nav navbar-right">
-                    <li class="dropdown"><a href="#" data-toggle="dropdown"
-                            class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                            <img alt="image" style="margin-bottom:4px !important;"
-                                src="./assets/stisla-assets/img/avatar/avatar-2.png"
-                                class="rounded-circle mr-1 my-auto border-white">
+                    <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                            <img alt="image" style="margin-bottom:4px !important;" src="./assets/stisla-assets/img/avatar/avatar-2.png" class="rounded-circle mr-1 my-auto border-white">
                             <div class="d-sm-none d-lg-inline-block" style="font-size:15px;">Hello, <?php
-$data['user'] = $this->db->get_where('admin', ['email' =>
-    $this->session->userdata('email')])->row_array();
-echo $data['user']['username'];
-?></div>
+                                                                                                    $data['user'] = $this->db->get_where('admin', ['email' =>
+                                                                                                    $this->session->userdata('email')])->row_array();
+                                                                                                    echo $data['user']['username'];
+                                                                                                    ?></div>
                         </a>
                         <div class="dropdown-menu dropdown-menu-right">
                             <div class="dropdown-title">Admin - Learnify</div>
-                            <a href="<?=base_url('welcome/logout')?>" class="dropdown-item has-icon text-danger">
+                            <a href="<?= base_url('welcome/logout') ?>" class="dropdown-item has-icon text-danger">
                                 <i class="fas fa-sign-out-alt"></i> Logout
                             </a>
                         </div>
@@ -70,28 +63,24 @@ echo $data['user']['username'];
                 <aside id="sidebar-wrapper">
                     <div class="sidebar-brand text-danger">
                         <div>
-                            <a href="<?=base_url('admin')?>"
-                                style="font-size: 35px;font-weight:900;font-family: 'Poppins', sans-serif;"
-                                class="text-success text-center"><i style="font-size: 30px;"
-                                    class="fas fa-graduation-cap"></i> |
+                            <a href="<?= base_url('admin') ?>" style="font-size: 35px;font-weight:900;font-family: 'Poppins', sans-serif;" class="text-success text-center"><i style="font-size: 30px;" class="fas fa-graduation-cap"></i> |
                                 Learnify</a>
                         </div>
                     </div>
                     <div class="sidebar-brand sidebar-brand-sm">
-                        <a href="<?=base_url('admin')?>">LY</a>
+                        <a href="<?= base_url('admin') ?>">LY</a>
                     </div>
                     <ul class="sidebar-menu">
                         <li class="menu-header ">Dashboard</li>
                         <li class="nav-item dropdown active">
-                            <a href="<?=base_url('admin')?>" class="nav-link"><i
-                                    class="fas fa-desktop"></i><span>Dashboard</span></a>
+                            <a href="<?= base_url('admin') ?>" class="nav-link"><i class="fas fa-desktop"></i><span>Dashboard</span></a>
                         </li>
                         <li class="menu-header">Management Siswa</li>
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i>
                                 <span>Siswa</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?=base_url('admin/data_siswa')?>">Data Siswa</a></li>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_siswa') ?>">Data Siswa</a></li>
                             </ul>
                         </li>
                         <li class="menu-header">Management Guru</li>
@@ -99,9 +88,9 @@ echo $data['user']['username'];
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-chalkboard-teacher"></i>
                                 <span>Guru</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?=base_url('admin/data_guru')?>">Data Guru</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_guru') ?>">Data Guru</a>
                                 </li>
-                                <li><a class="nav-link" href="<?=base_url('admin/add_guru')?>">Tambah Data Guru</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/add_guru') ?>">Tambah Data Guru</a>
                                 </li>
                             </ul>
                         </li>
@@ -110,9 +99,9 @@ echo $data['user']['username'];
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-book"></i>
                                 <span>Materi</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?=base_url('admin/data_materi')?>">Data Materi</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/data_materi') ?>">Data Materi</a>
                                 </li>
-                                <li><a class="nav-link" href="<?=base_url('admin/tambah_materi')?>">Tambah Materi</a>
+                                <li><a class="nav-link" href="<?= base_url('admin/tambah_materi') ?>">Tambah Materi</a>
                                 </li>
                             </ul>
                         </li>
@@ -121,10 +110,10 @@ echo $data['user']['username'];
                             <a href="#" class="nav-link has-dropdown"><i class="fas fa-address-card"></i>
                                 <span>Developer</span></a>
                             <ul class="dropdown-menu">
-                                <li><a class="nav-link" href="<?=base_url('admin/about_developer')?>">Tentang
+                                <li><a class="nav-link" href="<?= base_url('admin/about_developer') ?>">Tentang
                                         Pembuat</a>
                                 </li>
-                                <li><a class="nav-link" href="<?=base_url('admin/about_learnify')?>">Tentang
+                                <li><a class="nav-link" href="<?= base_url('admin/about_learnify') ?>">Tentang
                                         Learnify</a>
                                 </li>
                             </ul>
@@ -206,62 +195,54 @@ echo $data['user']['username'];
                             <div class="card-body">
                                 <h2 class="card-title" style="color: black;">Tambah Materi?</h2>
                                 <hr>
-                                <p class="card-text">Klik tombol dibawah untuk menambah materi . Materi yang
-                                    ditambahkan, akan langsung terupload di database Learnify. Dan para siswa bisa
-                                    segera belajar! </p>
-                                <a href="<?=base_url('admin/tambah_materi')?>" class="btn btn-primary">Tambah Materi</a>
+                                <p class="card-text">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
+                                <a href="<?= base_url('admin/tambah_materi') ?>" class="btn btn-primary">Tambah Materi ⭢</a>
                             </div>
                         </div>
                     </div>
                     <div class="">
-                        <div class="hero text-white hero-bg-image"
-                            data-background="<?=base_url('assets/')?>stisla-assets/img/unsplash/eberhard-grossgasteiger-1207565-unsplash.jpg">
+                        <div class="hero text-white hero-bg-image" data-background="<?= base_url('assets/') ?>stisla-assets/img/unsplash/eberhard-grossgasteiger-1207565-unsplash.jpg">
                             <div class=" hero-inner">
                                 <h1>Selamat Datang, <?php
-$data['user'] = $this->db->get_where('admin', ['email' =>
-    $this->session->userdata('email')])->row_array();
-echo $data['user']['username'];
-?>!</h1>
-                                <p class="lead">Di halaman admin Learnify - Web Edukasi Sekolah Menengah Kejuruan Satu
-                                    Ciamis.<br></p>
+                                                    $data['user'] = $this->db->get_where('admin', ['email' =>
+                                                    $this->session->userdata('email')])->row_array();
+                                                    echo $data['user']['username'];
+                                                    ?>!</h1>
+                                <p class="lead">After I ran into Helen at a restaurant, I realized she was just office pretty drop-dead date put in in a deck for our standup today. Who's responsible for the ask for this request? but moving the goalposts gain traction.</p>
                                 <div class="mt-4">
-                                    <a href="<?=base_url('admin/data_siswa')?>"
-                                        class="btn btn-outline-white btn-lg btn-icon icon-left"><i
-                                            class="far fa-user"></i>
+                                    <a href="<?= base_url('admin/data_siswa') ?>" class="btn btn-outline-white btn-lg btn-icon icon-left"><i class="far fa-user"></i>
                                         Data Siswa</a>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </section>
             </div>
+
+            <!-- Start Footer -->
             <footer class="main-footer">
-                <div class="footer-left">
-                    Copyright &copy; 2020 <div class="bullet"></div> Code & Design by <a
-                        href="https://syauqizaidan.github.io/">Syaauqi</a>
-                </div>
-                <div class="footer-right">
-                    Made with <span class="text-danger"> &#10084;</span> by Syaauqi
+                <div class="text-center">
+                    Copyright &copy; 2020 <div class="bullet"></div><a href="https://github.com/syauqi">Syauqi Zaidan Khairan Khalaf</a>
                 </div>
             </footer>
+            <!-- End Footer -->
+
         </div>
     </div>
     <!-- End Main Content -->
 
     <!-- General JS Scripts -->
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
-        integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-    <script src="<?=base_url('assets/')?>stisla-assets/js/stisla.js"></script>
+    <script src="<?= base_url('assets/') ?>stisla-assets/js/stisla.js"></script>
     <!-- Template JS File -->
-    <script src="<?=base_url('assets/')?>stisla-assets/js/scripts.js"></script>
-    <script src="<?=base_url('assets/')?>stisla-assets/js/custom.js"></script>
+    <script src="<?= base_url('assets/') ?>stisla-assets/js/scripts.js"></script>
+    <script src="<?= base_url('assets/') ?>stisla-assets/js/custom.js"></script>
 </body>
 
 </html>
