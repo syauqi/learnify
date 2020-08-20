ac7e66526bc4bedacbe950a20e78b957b23318dd
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-08-20 11:25:35 +0700

    Adding changes to email index 🎊

--- application/views/template/nav.php
+++ application/views/template/nav.php
@@ -20,30 +20,30 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="<?=base_url('assets/')?>img/favicon.png" type="image/png">
+    <link rel="icon" href="<?= base_url('assets/') ?>img/favicon.png" type="image/png">
     <title>Learnify - Belajar Dimana Saja & Kapan Saja !</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?=base_url('assets/')?>css/bootstrap.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/linericon/style.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/owl-carousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/lightbox/simpleLightbox.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/nice-select/css/nice-select.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/animate-css/animate.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>vendors/popup/magnific-popup.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/linericon/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/owl-carousel/owl.carousel.min.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/lightbox/simpleLightbox.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/nice-select/css/nice-select.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/animate-css/animate.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>vendors/popup/magnific-popup.css">
     <!-- main css -->
-    <link rel="stylesheet" href="<?=base_url('assets/')?>css/style.css">
-    <link rel="stylesheet" href="<?=base_url('assets/')?>css/responsive.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/style.css">
+    <link rel="stylesheet" href="<?= base_url('assets/') ?>css/responsive.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.10.4/dist/sweetalert2.all.min.js"></script>
-    <script src="<?=base_url('assets/')?>js/jquery-3.3.1.min.js"></script>
-    <script src="<?=base_url('assets/')?>js/popper.js"></script>
-    <script src="<?=base_url('assets/')?>js/bootstrap.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/jquery-3.3.1.min.js"></script>
+    <script src="<?= base_url('assets/') ?>js/popper.js"></script>
+    <script src="<?= base_url('assets/') ?>js/bootstrap.min.js"></script>
     <script type="text/javascript">
-      $(document).ready(()=>{
-        $("#nav<?=$this->uri->segment(2);?>").addClass('active')
-      })
+        $(document).ready(() => {
+            $("#nav<?= $this->uri->segment(2); ?>").addClass('active')
+        })
     </script>
 </head>
 
@@ -62,8 +62,7 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                     </ul>
                 </div>
                 <div class="float-right">
-                    <a class="dn_btn" href="tel:+6287830016267">087830016267</a>
-                    <a class="dn_btn" href="mailto:learnifyedu@gmail.com">learnifyedu@gmail.com</a>
+                    <a class="dn_btn" href="mailto:apps.learnify@gmail.com">apps.learnify@gmail.com</a>
                 </div>
             </div>
         </div>
@@ -71,11 +70,8 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
             <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container">
                     <!-- Brand and toggle get grouped for better mobile display -->
-                    <a class="navbar-brand logo_h" href="<?=base_url('welcome')?>"><img
-                            src="<?=base_url('assets/')?>img/logo.png" alt=""></a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
+                    <a class="navbar-brand logo_h" href="<?= base_url('welcome') ?>"><img src="<?= base_url('assets/') ?>img/logo.png" alt=""></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -83,17 +79,15 @@ dan guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
                         <ul class="nav navbar-nav menu_nav ml-auto">
-                            <li class="nav-item" id="nav"><a class="nav-link" href="<?=base_url('welcome')?>">Beranda</a></li>
-                            <li class="nav-item" id="navtentang"><a class="nav-link" href="<?=base_url('welcome/tentang')?>">Tentang</a>
+                            <li class="nav-item" id="nav"><a class="nav-link" href="<?= base_url('welcome') ?>">Beranda</a></li>
+                            <li class="nav-item" id="navtentang"><a class="nav-link" href="<?= base_url('welcome/tentang') ?>">Tentang</a>
                             </li>
                             <li class="nav-item submenu dropdown" id="navpelajaran">
-                                <a href="<?=base_url('welcome/pelajaran')?>" class="nav-link dropdown-toggle"
-                                    role="button" aria-haspopup="true" aria-expanded="false">Pelajaran</a>
+                                <a href="<?= base_url('welcome/pelajaran') ?>" class="nav-link dropdown-toggle" role="button" aria-haspopup="true" aria-expanded="false">Pelajaran</a>
                             </li>
-                            <li class="nav-item" id="navkontak"><a class="nav-link" href="<?=base_url('welcome/kontak')?>">Kontak</a>
+                            <li class="nav-item" id="navkontak"><a class="nav-link" href="<?= base_url('welcome/kontak') ?>">Kontak</a>
                             </li>
-                            <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal"
-                                    data-target="#exampleModalCenter">Masuk</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">Masuk</a></li>
                         </ul>
                     </div>
                 </div>
