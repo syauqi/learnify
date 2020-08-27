97e427027982b29e051882d2dc413075f6c99bf5
Syauqi Zaidan Khairan Khalaf <syaokay@gmail.com>
2020-08-27 22:00:34 +0700

    Adding major changes kontak and pelajaran page 💤

--- application/views/pelajaran.php
+++ application/views/pelajaran.php
@@ -1,17 +1,3 @@
-<!--
-@Project: Learnify
-@Programmer: Syauqi Zaidan Khairan Khalaf
-@Website: https://linktr.ee/syauqi
-@Email : syaokay@gmail.com
-
-@About-Learnify :
-Web Edukasi Open Source yang dibuat oleh Syauqi Zaidan Khairan Khalaf.
-Learnify adalah Web edukasi yang dilengkapi video, materi dan sistem ujian
-yang tersedia secara gratis. Learnify dibuat ditujukan agar para siswa dan
-guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
--->
-
-
 <!--================Home Banner Area =================-->
 <section class="banner_area">
     <div class="banner_inner d-flex align-items-center">
@@ -19,8 +5,8 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
         </div>
         <div class="container">
             <div class="banner_content text-center">
-                <h2>Pelajaran</h2>
-                <div class="page_link">
+                <h2 data-aos="fade-up" data-aos-duration="1600">Pelajaran</h2>
+                <div data-aos="fade-up" data-aos-duration="1800" class="page_link">
                     <a href="<?= base_url('welcome') ?>">Beranda</a>
                     <a href="<?= base_url('welcome/pelajaran') ?>">Pelajaran</a>
                 </div>
@@ -30,29 +16,26 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
 </section>
 <!--================End Home Banner Area =================-->
 
-
 <!--================Courses Area =================-->
-<section class="courses_area p_120">
+<section class="courses_area p_40">
     <div class="container">
         <div class="main_title">
-            <h2>Pelajaran Yang Tersedia</h2>
-            <p>Dibawah ini merupakan mata pelajaran yang tersedia di website Learnify. Tiap kelas mempunyai
-                materi yang berbeda namun dengan mata pelajaran yang sama. Oleh karena itu nikmati materi dan selamat
-                belajar! tunggu update selanjutnya untuk penambahan mata pelajaran!</p>
+            <h2 data-aos="fade-up" data-aos-duration="1600">Pelajaran Yang Tersedia di Learnify</h2>
+            <p data-aos="fade-up" data-aos-duration="1800">Merciful revaluation burying love ultimate value inexpedient ubermensch. Holiest madness victorious morality hope endless christian madness. Love dead fearful transvaluation marvelous. Oneself right ideal abstract endless faith deceptions zarathustra grandeur law ubermensch free.</p>
         </div>
         <div class="row courses_inner">
             <div class="col-lg-9">
                 <div class="grid_inner">
                     <div class="grid_item wd55">
-                        <div class="courses_item">
+                        <div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
                             <img src="<?= base_url('assets/') ?>img/courses/course-1.jpg" alt="">
                             <div class="hover_text">
                                 <a class="cat" href="#">Gratis</a>
-                                <a href="#">
+                                <a href="javaScript:void(0);">
                                     <h4>Kelas Matematika Gratis</h4>
                                 </a>
                                 <ul class="list">
-                                    <li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+                                    <li><a href="#"><i class="lnr lnr-users"></i>54</a></li>
                                     <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
                                     <li><a href="#"><i class="lnr lnr-user"></i>Guru Matematika SMKN 1 Ciamis</a>
                                     </li>
@@ -61,11 +44,11 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         </div>
                     </div>
                     <div class="grid_item wd44">
-                        <div class="courses_item">
+                        <div class="courses_item" data-aos="fade-down" data-aos-duration="1800">
                             <img src="<?= base_url('assets/') ?>img/courses/course-2.jpg" alt="">
                             <div class="hover_text">
                                 <a class="cat" href="#">Gratis</a>
-                                <a href="#">
+                                <a href="javaScript:void(0);">
                                     <h4>Kelas IPA Gratis</h4>
                                 </a>
                                 <ul class="list">
@@ -77,15 +60,15 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         </div>
                     </div>
                     <div class="grid_item wd44">
-                        <div class="courses_item">
+                        <div class="courses_item" data-aos="fade-right" data-aos-duration="1800">
                             <img src="<?= base_url('assets/') ?>img/courses/course-4.jpg" alt="">
                             <div class="hover_text">
                                 <a class="cat" href="#">Gratis</a>
-                                <a href="#">
+                                <a href="javaScript:void(0);">
                                     <h4>Kelas Bahasa Inggris Gratis</h4>
                                 </a>
                                 <ul class="list">
-                                    <li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 63</a></li>
                                     <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
                                     <li><a href="#"><i class="lnr lnr-user"></i> Guru English SMKN 1 Ciamis</a></li>
                                 </ul>
@@ -93,15 +76,15 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                         </div>
                     </div>
                     <div class="grid_item wd55">
-                        <div class="courses_item">
+                        <div class="courses_item" data-aos="fade-up" data-aos-duration="1800">
                             <img src="<?= base_url('assets/') ?>img/courses/course-5.jpg" alt="">
                             <div class="hover_text">
                                 <a class="cat" href="#">Gratis</a>
-                                <a href="#">
+                                <a href="javaScript:void(0);">
                                     <h4>Kelas Bahasa Indonesia Gratis</h4>
                                 </a>
                                 <ul class="list">
-                                    <li><a href="#"><i class="lnr lnr-users"></i> 34</a></li>
+                                    <li><a href="#"><i class="lnr lnr-users"></i> 24</a></li>
                                     <li><a href="#"><i class="lnr lnr-bubble"></i> 0</a></li>
                                     <li><a href="#"><i class="lnr lnr-user"></i> Guru Indonesia SMKN 1 Ciamis</a>
                                     </li>
@@ -112,11 +95,11 @@ guru dapat terus belajar dan mengajar dimana saja dan kapan saja.
                 </div>
             </div>
             <div class="col-lg-3">
-                <div class="course_item">
+                <div class="course_item" data-aos="fade-left" data-aos-duration="1800">
                     <img src="<?= base_url('assets/') ?>img/courses/course-3.jpg" alt="">
                     <div class="hover_text">
                         <a class="cat" href="#">Gratis</a>
-                        <a href="#">
+                        <a href="javaScript:void(0);">
                             <h4>Kelas Pendidikan Agama Islam Gratis</h4>
                         </a>
                         <ul class="list">
